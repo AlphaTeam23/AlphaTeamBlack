8f911f2dbaa435cdfe9561b011dd1c7c358a351c
Massiel <massielrodriguez990@gmail.com>
2024-05-07 13:58:15 -0400

    comente los titulos del codigo js

--- js/script.js
+++ js/script.js
@@ -6,13 +6,13 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
 
     // Verificar el rol del usuario
     if (username === "admin" && password === "admin123") {
-        // Redirigir al panel de administrador
+        // Perfil de administrador
         window.location.href = "a_alphaTeam.html";
     } else if (username === "estudiante" && password === "estudiante123") {
-        // Redirigir al panel de estudiante
+        // Perfil de estudiante
         window.location.href = "e_alphaTeam.html";
     } else if (username === "profesor" && password === "profesor123") {
-        // Redirigir al panel de profesor
+        // Perfil de profesor
         window.location.href = "p_alphaTeam.html";
     } else {
         // Mostrar un mensaje de error si las credenciales son incorrectas
