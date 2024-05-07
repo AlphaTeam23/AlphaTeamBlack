29dbc86f973435d9bd2a467dbf59bab612f7caa0
Massiel <massielrodriguez990@gmail.com>
2024-05-07 13:49:08 -0400

    arregle boton acceder y conecte paginas para validacion

--- js/script.js
+++ js/script.js
@@ -10,10 +10,10 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
         window.location.href = "a_alphaTeam.html";
     } else if (username === "estudiante" && password === "estudiante123") {
         // Redirigir al panel de estudiante
-        window.location.href = "panel-estudiante.html";
+        window.location.href = "e_alphaTeam.html";
     } else if (username === "profesor" && password === "profesor123") {
         // Redirigir al panel de profesor
-        window.location.href = "panel-profesor.html";
+        window.location.href = "p_alphaTeam.html";
     } else {
         // Mostrar un mensaje de error si las credenciales son incorrectas
         alert("Nombre de usuario o contraseña incorrectos.");
