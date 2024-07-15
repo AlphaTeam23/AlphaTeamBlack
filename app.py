caec777a6c0dc890ea938db935717277b6557cd0
Jose <Josemiguelreyes03@gmail.com>
2024-07-15 14:12:01 -0400

    Cambio de cerrar sesion

--- app.py
+++ app.py
@@ -112,7 +112,10 @@ def a_usuarios():
 
 @app.route('/alphaTeam/templates/cerrarsesion')
 def p_cerrar():
-    return render_template('./index.html')
+    session.clear()
+    return redirect('./index.html')
+
+
 
 if __name__ == '__main__':
     app.run( port = 5501, debug=True)
