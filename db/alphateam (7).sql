25b7ceadfd857bd36c1fdf93a599832d62397ed2
Massiel <massielrodriguez990@gmail.com>
2024-08-19 17:47:00 -0400

    actualice base de datos

--- db/alphateam (7).sql
+++ db/alphateam (7).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-08-2024 a las 22:49:55
+-- Tiempo de generación: 19-08-2024 a las 23:46:09
 -- Versión del servidor: 10.1.37-MariaDB
 -- Versión de PHP: 7.2.13
 
@@ -144,7 +144,7 @@ INSERT INTO `dias` (`id_dias`, `dias`) VALUES
 
 CREATE TABLE `estudiante` (
   `id_estudiante` int(5) NOT NULL,
-  `e_Matricula` char(6) COLLATE utf32_spanish2_ci NOT NULL,
+  `matricula` char(6) COLLATE utf32_spanish2_ci NOT NULL,
   `nombre_estudiante` varchar(30) COLLATE utf32_spanish2_ci NOT NULL,
   `apellidos` varchar(30) COLLATE utf32_spanish2_ci DEFAULT NULL,
   `edad` int(3) NOT NULL,
@@ -154,43 +154,6 @@ CREATE TABLE `estudiante` (
   `id_profesor` int(5) NOT NULL,
   `id_tutor` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_spanish2_ci;
-
---
--- Volcado de datos para la tabla `estudiante`
---
-
-INSERT INTO `estudiante` (`id_estudiante`, `e-Matricula`, `nombre_estudiante`, `apellidos`, `edad`, `sexo_estudiante`, `nacimiento_estudiante`, `id_curso`, `id_profesor`, `id_tutor`) VALUES
-(1, '', 'Faury', 'García Rodríguez', 0, 'Masculino', '2004-09-17', 4, 4, 2),
-(2, '', 'Jose Miguel', 'Bello Acosta', 0, 'Masculino', '2003-09-04', 4, 4, 4),
-(3, '', 'Jose Miguel', 'Reyes', 0, 'Masculino', '1996-11-08', 4, 4, 5),
-(4, '', 'Massiel', 'Rodriguez Rodríguez', 0, 'Femenino', '2004-01-04', 4, 4, 8),
-(5, '', 'Manuel De Jesus', 'Díaz Díaz', 0, 'Masculino', '1999-01-13', 4, 4, 6),
-(6, '', 'Albiery', 'Rodriguez Almonte', 0, 'Masculino', '2004-08-19', 5, 5, 1),
-(7, '', 'Smailyn', 'Gutiérrez Burgos', 0, 'Femenino', '2003-03-04', 5, 5, 9),
-(8, '', 'Gissel Esmeralda', 'Velez Cruz', 0, 'Femenino', '1999-09-27', 5, 5, 3),
-(9, '', 'Marilin', 'Gil Paulino', 0, 'Femenino', '2005-01-28', 5, 5, 7),
-(10, '', 'Tiara', 'Peña Rodríguez', 0, 'Femenino', '2003-12-11', 5, 5, 12),
-(11, '', 'Ana', 'Martínez', 0, 'Femenino', '2001-03-15', 6, 6, 11),
-(12, '', 'Luis', 'García', 0, 'Masculino', '1999-06-21', 1, 1, 13),
-(13, '', 'Marta', 'Fernández', 0, 'Femenino', '2000-08-30', 6, 6, 15),
-(14, '', 'Carlos', 'López', 0, 'Masculino', '2002-11-05', 6, 6, 18),
-(15, '', 'Sofía', 'Gómez', 0, 'Femenino', '2003-12-18', 6, 6, 20),
-(16, '', 'Jorge', 'Sánchez', 0, 'Masculino', '1998-02-25', 2, 2, 22),
-(17, '', 'Laura', 'Martínez', 0, 'Femenino', '2004-04-10', 2, 2, 25),
-(18, '', 'Pedro', 'Ramírez', 0, 'Masculino', '2002-07-22', 2, 2, 27),
-(19, '', 'Isabel', 'Hernández', 0, 'Femenino', '2001-09-14', 2, 2, 29),
-(20, '', 'Felipe', 'Morales', 0, 'Masculino', '2000-10-30', 2, 2, 30),
-(21, '', 'Catalina', 'Cano', 0, 'Femenino', '2003-01-05', 3, 3, 21),
-(22, '', 'Antonio', 'Gómez', 0, 'Masculino', '2002-03-18', 3, 3, 23),
-(23, '', 'Elena', 'Castro', 0, 'Femenino', '1999-05-27', 3, 3, 24),
-(24, '', 'Alejandro', 'Ramírez', 0, 'Masculino', '2000-07-15', 3, 3, 26),
-(25, '', 'Patricia', 'Mendoza', 0, 'Femenino', '2001-09-04', 3, 3, 28),
-(26, '', 'Manuel', 'Serrano', 0, 'Masculino', '1999-02-20', 1, 1, 10),
-(27, '', 'Victoria', 'Álvarez', 0, 'Femenino', '2000-04-25', 1, 1, 14),
-(28, '', 'Julio', 'Paredes', 0, 'Masculino', '2001-06-30', 1, 1, 16),
-(29, '', 'Natalia', 'Mora', 0, 'Femenino', '2003-08-15', 1, 1, 17),
-(30, '', 'Diego', 'Velasco', 0, 'Masculino', '2002-10-12', 6, 6, 19),
-(31, '', 'Prueba', 'Prueba', 0, 'mas', '1996-08-11', 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -506,66 +469,6 @@ ALTER TABLE `horario`
 --
 ALTER TABLE `profesores`
   MODIFY `id_profesor` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT de la tabla `roles`
---
-ALTER TABLE `roles`
-  MODIFY `id_roles` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de la tabla `tutor`
---
-ALTER TABLE `tutor`
-  MODIFY `id_tutor` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `calificacion_final`
---
-ALTER TABLE `calificacion_final`
-  ADD CONSTRAINT `calificacion_final_ibfk_1` FOREIGN KEY (`id_asignatura`) REFERENCES `asignatura` (`id_asignatura`),
-  ADD CONSTRAINT `calificacion_final_ibfk_2` FOREIGN KEY (`id_estudiante`) REFERENCES `estudiante` (`id_estudiante`);
-
---
--- Filtros para la tabla `estudiante`
---
-ALTER TABLE `estudiante`
-  ADD CONSTRAINT `estudiante_ibfk_1` FOREIGN KEY (`id_tutor`) REFERENCES `tutor` (`id_tutor`),
-  ADD CONSTRAINT `estudiante_ibfk_3` FOREIGN KEY (`id_profesor`) REFERENCES `profesores` (`id_profesor`),
-  ADD CONSTRAINT `estudiante_ibfk_4` FOREIGN KEY (`id_curso`) REFERENCES `cursos` (`id_curso`),
-  ADD CONSTRAINT `estudiante_ibfk_5` FOREIGN KEY (`id_profesor`) REFERENCES `profesores` (`id_profesor`);
-
---
--- Filtros para la tabla `horario`
---
-ALTER TABLE `horario`
-  ADD CONSTRAINT `horario_ibfk_1` FOREIGN KEY (`id_hora`) REFERENCES `hora` (`id_hora`),
-  ADD CONSTRAINT `horario_ibfk_2` FOREIGN KEY (`id_profesor`) REFERENCES `profesores` (`id_profesor`),
-  ADD CONSTRAINT `horario_ibfk_3` FOREIGN KEY (`id_curso`) REFERENCES `cursos` (`id_curso`),
-  ADD CONSTRAINT `horario_ibfk_4` FOREIGN KEY (`id_asignatura`) REFERENCES `asignatura` (`id_asignatura`),
-  ADD CONSTRAINT `horario_ibfk_5` FOREIGN KEY (`id_dias`) REFERENCES `dias` (`id_dias`);
-
---
--- Filtros para la tabla `profesores`
---
-ALTER TABLE `profesores`
-  ADD CONSTRAINT `profesores_ibfk_1` FOREIGN KEY (`id_asignatura`) REFERENCES `asignatura` (`id_asignatura`);
-
---
--- Filtros para la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_roles`) REFERENCES `roles` (`id_roles`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
