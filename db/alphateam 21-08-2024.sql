1988f09bc28587725739ac98e4edc9fb993ba25a
Massiel <massielrodriguez990@gmail.com>
2024-08-21 14:49:37 -0400

    actualice base de datos

--- db/alphateam 21-08-2024.sql
+++ db/alphateam 21-08-2024.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-08-2024 a las 23:46:09
+-- Tiempo de generación: 21-08-2024 a las 20:48:27
 -- Versión del servidor: 10.1.37-MariaDB
 -- Versión de PHP: 7.2.13
 
@@ -62,7 +62,223 @@ CREATE TABLE `calificaciones` (
   `participacion` int(2) NOT NULL,
   `asistencia` int(2) NOT NULL,
   `cali_final` int(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `calificaciones`
+--
+
+INSERT INTO `calificaciones` (`id_calificaciones`, `id_asignatura`, `id_estudiante`, `tareas`, `examenes`, `participacion`, `asistencia`, `cali_final`) VALUES
+(1, 1, 1, 0, 0, 0, 0, 0),
+(2, 2, 1, 0, 0, 0, 0, 0),
+(3, 3, 1, 0, 0, 0, 0, 0),
+(4, 4, 1, 0, 0, 0, 0, 0),
+(5, 5, 1, 0, 0, 0, 0, 0),
+(6, 6, 1, 0, 0, 0, 0, 0),
+(7, 7, 1, 0, 0, 0, 0, 0),
+(8, 1, 2, 0, 0, 0, 0, 0),
+(9, 2, 2, 0, 0, 0, 0, 0),
+(11, 3, 2, 0, 0, 0, 0, 0),
+(12, 4, 2, 0, 0, 0, 0, 0),
+(13, 5, 2, 0, 0, 0, 0, 0),
+(14, 6, 2, 0, 0, 0, 0, 0),
+(15, 7, 2, 0, 0, 0, 0, 0),
+(16, 1, 3, 0, 0, 0, 0, 0),
+(17, 2, 3, 0, 0, 0, 0, 0),
+(18, 3, 3, 0, 0, 0, 0, 0),
+(19, 4, 3, 0, 0, 0, 0, 0),
+(20, 5, 3, 0, 0, 0, 0, 0),
+(21, 6, 3, 0, 0, 0, 0, 0),
+(22, 7, 3, 0, 0, 0, 0, 0),
+(23, 1, 4, 0, 0, 0, 0, 0),
+(24, 2, 4, 0, 0, 0, 0, 0),
+(25, 3, 4, 0, 0, 0, 0, 0),
+(26, 4, 4, 0, 0, 0, 0, 0),
+(27, 5, 4, 0, 0, 0, 0, 0),
+(28, 6, 4, 0, 0, 0, 0, 0),
+(29, 7, 4, 0, 0, 0, 0, 0),
+(30, 1, 5, 0, 0, 0, 0, 0),
+(31, 2, 5, 0, 0, 0, 0, 0),
+(32, 3, 5, 0, 0, 0, 0, 0),
+(33, 4, 5, 0, 0, 0, 0, 0),
+(34, 5, 5, 0, 0, 0, 0, 0),
+(35, 6, 5, 0, 0, 0, 0, 0),
+(36, 7, 5, 0, 0, 0, 0, 0),
+(37, 1, 6, 0, 0, 0, 0, 0),
+(38, 2, 6, 0, 0, 0, 0, 0),
+(39, 3, 6, 0, 0, 0, 0, 0),
+(40, 4, 6, 0, 0, 0, 0, 0),
+(41, 5, 6, 0, 0, 0, 0, 0),
+(42, 6, 6, 0, 0, 0, 0, 0),
+(43, 7, 6, 0, 0, 0, 0, 0),
+(44, 1, 7, 0, 0, 0, 0, 0),
+(45, 2, 7, 0, 0, 0, 0, 0),
+(46, 3, 7, 0, 0, 0, 0, 0),
+(47, 4, 7, 0, 0, 0, 0, 0),
+(48, 5, 7, 0, 0, 0, 0, 0),
+(49, 6, 7, 0, 0, 0, 0, 0),
+(50, 7, 7, 0, 0, 0, 0, 0),
+(51, 1, 8, 0, 0, 0, 0, 0),
+(52, 2, 8, 0, 0, 0, 0, 0),
+(53, 3, 8, 0, 0, 0, 0, 0),
+(54, 4, 8, 0, 0, 0, 0, 0),
+(55, 5, 8, 0, 0, 0, 0, 0),
+(56, 6, 8, 0, 0, 0, 0, 0),
+(57, 7, 8, 0, 0, 0, 0, 0),
+(58, 1, 9, 0, 0, 0, 0, 0),
+(59, 2, 9, 0, 0, 0, 0, 0),
+(60, 3, 9, 0, 0, 0, 0, 0),
+(61, 4, 9, 0, 0, 0, 0, 0),
+(62, 5, 9, 0, 0, 0, 0, 0),
+(63, 6, 9, 0, 0, 0, 0, 0),
+(64, 7, 9, 0, 0, 0, 0, 0),
+(65, 1, 10, 0, 0, 0, 0, 0),
+(66, 2, 10, 0, 0, 0, 0, 0),
+(67, 3, 10, 0, 0, 0, 0, 0),
+(68, 4, 10, 0, 0, 0, 0, 0),
+(69, 5, 10, 0, 0, 0, 0, 0),
+(70, 6, 10, 0, 0, 0, 0, 0),
+(71, 7, 10, 0, 0, 0, 0, 0),
+(72, 1, 11, 0, 0, 0, 0, 0),
+(73, 2, 11, 0, 0, 0, 0, 0),
+(74, 3, 11, 0, 0, 0, 0, 0),
+(75, 4, 11, 0, 0, 0, 0, 0),
+(76, 5, 11, 0, 0, 0, 0, 0),
+(77, 6, 11, 0, 0, 0, 0, 0),
+(78, 7, 11, 0, 0, 0, 0, 0),
+(79, 1, 12, 0, 0, 0, 0, 0),
+(80, 2, 12, 0, 0, 0, 0, 0),
+(81, 3, 12, 0, 0, 0, 0, 0),
+(82, 4, 12, 0, 0, 0, 0, 0),
+(83, 5, 12, 0, 0, 0, 0, 0),
+(84, 6, 12, 0, 0, 0, 0, 0),
+(85, 7, 12, 0, 0, 0, 0, 0),
+(86, 1, 13, 0, 0, 0, 0, 0),
+(87, 2, 13, 0, 0, 0, 0, 0),
+(88, 3, 13, 0, 0, 0, 0, 0),
+(89, 4, 13, 0, 0, 0, 0, 0),
+(90, 5, 13, 0, 0, 0, 0, 0),
+(91, 6, 13, 0, 0, 0, 0, 0),
+(92, 7, 13, 0, 0, 0, 0, 0),
+(93, 1, 14, 0, 0, 0, 0, 0),
+(94, 2, 14, 0, 0, 0, 0, 0),
+(95, 3, 14, 0, 0, 0, 0, 0),
+(96, 4, 14, 0, 0, 0, 0, 0),
+(97, 5, 14, 0, 0, 0, 0, 0),
+(98, 6, 14, 0, 0, 0, 0, 0),
+(99, 7, 14, 0, 0, 0, 0, 0),
+(100, 1, 15, 0, 0, 0, 0, 0),
+(101, 2, 15, 0, 0, 0, 0, 0),
+(102, 3, 15, 0, 0, 0, 0, 0),
+(103, 4, 15, 0, 0, 0, 0, 0),
+(104, 5, 15, 0, 0, 0, 0, 0),
+(105, 6, 15, 0, 0, 0, 0, 0),
+(106, 7, 15, 0, 0, 0, 0, 0),
+(107, 1, 16, 0, 0, 0, 0, 0),
+(108, 2, 16, 0, 0, 0, 0, 0),
+(109, 3, 16, 0, 0, 0, 0, 0),
+(110, 4, 16, 0, 0, 0, 0, 0),
+(111, 5, 16, 0, 0, 0, 0, 0),
+(112, 6, 16, 0, 0, 0, 0, 0),
+(113, 7, 16, 0, 0, 0, 0, 0),
+(114, 1, 17, 0, 0, 0, 0, 0),
+(115, 2, 17, 0, 0, 0, 0, 0),
+(116, 3, 17, 0, 0, 0, 0, 0),
+(117, 4, 17, 0, 0, 0, 0, 0),
+(118, 5, 17, 0, 0, 0, 0, 0),
+(119, 6, 17, 0, 0, 0, 0, 0),
+(120, 7, 17, 0, 0, 0, 0, 0),
+(121, 1, 18, 0, 0, 0, 0, 0),
+(122, 2, 18, 0, 0, 0, 0, 0),
+(123, 3, 18, 0, 0, 0, 0, 0),
+(124, 4, 18, 0, 0, 0, 0, 0),
+(125, 5, 18, 0, 0, 0, 0, 0),
+(126, 6, 18, 0, 0, 0, 0, 0),
+(127, 7, 18, 0, 0, 0, 0, 0),
+(128, 1, 19, 0, 0, 0, 0, 0),
+(129, 2, 19, 0, 0, 0, 0, 0),
+(130, 3, 19, 0, 0, 0, 0, 0),
+(131, 4, 19, 0, 0, 0, 0, 0),
+(132, 5, 19, 0, 0, 0, 0, 0),
+(133, 6, 19, 0, 0, 0, 0, 0),
+(134, 7, 19, 0, 0, 0, 0, 0),
+(135, 1, 20, 0, 0, 0, 0, 0),
+(136, 2, 20, 0, 0, 0, 0, 0),
+(137, 3, 20, 0, 0, 0, 0, 0),
+(138, 4, 20, 0, 0, 0, 0, 0),
+(139, 5, 20, 0, 0, 0, 0, 0),
+(140, 6, 20, 0, 0, 0, 0, 0),
+(141, 7, 20, 0, 0, 0, 0, 0),
+(142, 1, 21, 0, 0, 0, 0, 0),
+(143, 2, 21, 0, 0, 0, 0, 0),
+(144, 3, 21, 0, 0, 0, 0, 0),
+(145, 4, 21, 0, 0, 0, 0, 0),
+(146, 5, 21, 0, 0, 0, 0, 0),
+(147, 6, 21, 0, 0, 0, 0, 0),
+(148, 7, 21, 0, 0, 0, 0, 0),
+(149, 1, 22, 0, 0, 0, 0, 0),
+(150, 2, 22, 0, 0, 0, 0, 0),
+(151, 3, 22, 0, 0, 0, 0, 0),
+(152, 4, 22, 0, 0, 0, 0, 0),
+(153, 5, 22, 0, 0, 0, 0, 0),
+(154, 6, 22, 0, 0, 0, 0, 0),
+(155, 7, 22, 0, 0, 0, 0, 0),
+(156, 1, 23, 0, 0, 0, 0, 0),
+(157, 2, 23, 0, 0, 0, 0, 0),
+(158, 3, 23, 0, 0, 0, 0, 0),
+(159, 4, 23, 0, 0, 0, 0, 0),
+(160, 5, 23, 0, 0, 0, 0, 0),
+(161, 6, 23, 0, 0, 0, 0, 0),
+(162, 7, 23, 0, 0, 0, 0, 0),
+(163, 1, 24, 0, 0, 0, 0, 0),
+(164, 2, 24, 0, 0, 0, 0, 0),
+(165, 3, 24, 0, 0, 0, 0, 0),
+(166, 4, 24, 0, 0, 0, 0, 0),
+(167, 5, 24, 0, 0, 0, 0, 0),
+(168, 6, 24, 0, 0, 0, 0, 0),
+(169, 7, 24, 0, 0, 0, 0, 0),
+(170, 1, 25, 0, 0, 0, 0, 0),
+(171, 2, 25, 0, 0, 0, 0, 0),
+(172, 3, 25, 0, 0, 0, 0, 0),
+(173, 4, 25, 0, 0, 0, 0, 0),
+(174, 5, 25, 0, 0, 0, 0, 0),
+(175, 6, 25, 0, 0, 0, 0, 0),
+(176, 7, 25, 0, 0, 0, 0, 0),
+(177, 1, 26, 0, 0, 0, 0, 0),
+(178, 2, 26, 0, 0, 0, 0, 0),
+(179, 3, 26, 0, 0, 0, 0, 0),
+(180, 4, 26, 0, 0, 0, 0, 0),
+(181, 5, 26, 0, 0, 0, 0, 0),
+(182, 6, 26, 0, 0, 0, 0, 0),
+(183, 7, 26, 0, 0, 0, 0, 0),
+(184, 1, 27, 0, 0, 0, 0, 0),
+(185, 2, 27, 0, 0, 0, 0, 0),
+(186, 3, 27, 0, 0, 0, 0, 0),
+(187, 4, 27, 0, 0, 0, 0, 0),
+(188, 5, 27, 0, 0, 0, 0, 0),
+(189, 6, 27, 0, 0, 0, 0, 0),
+(190, 7, 27, 0, 0, 0, 0, 0),
+(191, 1, 28, 0, 0, 0, 0, 0),
+(192, 2, 28, 0, 0, 0, 0, 0),
+(193, 3, 28, 0, 0, 0, 0, 0),
+(194, 4, 28, 0, 0, 0, 0, 0),
+(195, 5, 28, 0, 0, 0, 0, 0),
+(196, 6, 28, 0, 0, 0, 0, 0),
+(197, 7, 28, 0, 0, 0, 0, 0),
+(198, 1, 29, 0, 0, 0, 0, 0),
+(199, 2, 29, 0, 0, 0, 0, 0),
+(200, 3, 29, 0, 0, 0, 0, 0),
+(201, 4, 29, 0, 0, 0, 0, 0),
+(202, 5, 29, 0, 0, 0, 0, 0),
+(203, 6, 29, 0, 0, 0, 0, 0),
+(204, 7, 29, 0, 0, 0, 0, 0),
+(205, 1, 30, 0, 0, 0, 0, 0),
+(206, 2, 30, 0, 0, 0, 0, 0),
+(207, 3, 30, 0, 0, 0, 0, 0),
+(208, 4, 30, 0, 0, 0, 0, 0),
+(209, 5, 30, 0, 0, 0, 0, 0),
+(210, 6, 30, 0, 0, 0, 0, 0),
+(211, 7, 30, 0, 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -73,7 +289,6 @@ CREATE TABLE `calificaciones` (
 CREATE TABLE `calificacion_final` (
   `id_asignatura` int(5) NOT NULL,
   `id_estudiante` int(5) NOT NULL,
-  `id_calificaciones` int(5) NOT NULL,
   `primer_periodo` int(3) NOT NULL,
   `segundo_periodo` int(3) NOT NULL,
   `tecer_periodo` int(3) NOT NULL,
@@ -87,8 +302,8 @@ CREATE TABLE `calificacion_final` (
 -- Volcado de datos para la tabla `calificacion_final`
 --
 
-INSERT INTO `calificacion_final` (`id_asignatura`, `id_estudiante`, `id_calificaciones`, `primer_periodo`, `segundo_periodo`, `tecer_periodo`, `cuarto-periodo`, `completivo`, `extraordinario`, `final`) VALUES
-(1, 2, 2, 80, 80, 80, 80, NULL, NULL, 80);
+INSERT INTO `calificacion_final` (`id_asignatura`, `id_estudiante`, `primer_periodo`, `segundo_periodo`, `tecer_periodo`, `cuarto-periodo`, `completivo`, `extraordinario`, `final`) VALUES
+(1, 2, 80, 80, 80, 80, NULL, NULL, 80);
 
 -- --------------------------------------------------------
 
@@ -144,16 +359,51 @@ INSERT INTO `dias` (`id_dias`, `dias`) VALUES
 
 CREATE TABLE `estudiante` (
   `id_estudiante` int(5) NOT NULL,
-  `matricula` char(6) COLLATE utf32_spanish2_ci NOT NULL,
+  `e-Matricula` char(6) COLLATE utf32_spanish2_ci NOT NULL,
   `nombre_estudiante` varchar(30) COLLATE utf32_spanish2_ci NOT NULL,
   `apellidos` varchar(30) COLLATE utf32_spanish2_ci DEFAULT NULL,
-  `edad` int(3) NOT NULL,
   `sexo_estudiante` varchar(9) COLLATE utf32_spanish2_ci NOT NULL,
   `nacimiento_estudiante` date NOT NULL,
   `id_curso` int(5) NOT NULL,
   `id_profesor` int(5) NOT NULL,
   `id_tutor` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `estudiante`
+--
+
+INSERT INTO `estudiante` (`id_estudiante`, `e-Matricula`, `nombre_estudiante`, `apellidos`, `sexo_estudiante`, `nacimiento_estudiante`, `id_curso`, `id_profesor`, `id_tutor`) VALUES
+(1, '', 'Faury', 'García Rodríguez', 'Masculino', '2004-09-17', 4, 4, 2),
+(2, '', 'Jose Miguel', 'Bello Acosta', 'Masculino', '2003-09-04', 4, 4, 4),
+(3, '', 'Jose Miguel', 'Reyes', 'Masculino', '1996-11-08', 4, 4, 5),
+(4, '', 'Massiel', 'Rodriguez Rodríguez', 'Femenino', '2004-01-04', 4, 4, 8),
+(5, '', 'Manuel De Jesus', 'Díaz Díaz', 'Masculino', '1999-01-13', 4, 4, 6),
+(6, '', 'Albiery', 'Rodriguez Almonte', 'Masculino', '2004-08-19', 5, 5, 1),
+(7, '', 'Smailyn', 'Gutiérrez Burgos', 'Femenino', '2003-03-04', 5, 5, 9),
+(8, '', 'Gissel Esmeralda', 'Velez Cruz', 'Femenino', '1999-09-27', 5, 5, 3),
+(9, '', 'Marilin', 'Gil Paulino', 'Femenino', '2005-01-28', 5, 5, 7),
+(10, '', 'Tiara', 'Peña Rodríguez', 'Femenino', '2003-12-11', 5, 5, 12),
+(11, '', 'Ana', 'Martínez', 'Femenino', '2001-03-15', 6, 6, 11),
+(12, '', 'Luis', 'García', 'Masculino', '1999-06-21', 1, 1, 13),
+(13, '', 'Marta', 'Fernández', 'Femenino', '2000-08-30', 6, 6, 15),
+(14, '', 'Carlos', 'López', 'Masculino', '2002-11-05', 6, 6, 18),
+(15, '', 'Sofía', 'Gómez', 'Femenino', '2003-12-18', 6, 6, 20),
+(16, '', 'Jorge', 'Sánchez', 'Masculino', '1998-02-25', 2, 2, 22),
+(17, '', 'Laura', 'Martínez', 'Femenino', '2004-04-10', 2, 2, 25),
+(18, '', 'Pedro', 'Ramírez', 'Masculino', '2002-07-22', 2, 2, 27),
+(19, '', 'Isabel', 'Hernández', 'Femenino', '2001-09-14', 2, 2, 29),
+(20, '', 'Felipe', 'Morales', 'Masculino', '2000-10-30', 2, 2, 30),
+(21, '', 'Catalina', 'Cano', 'Femenino', '2003-01-05', 3, 3, 21),
+(22, '', 'Antonio', 'Gómez', 'Masculino', '2002-03-18', 3, 3, 23),
+(23, '', 'Elena', 'Castro', 'Femenino', '1999-05-27', 3, 3, 24),
+(24, '', 'Alejandro', 'Ramírez', 'Masculino', '2000-07-15', 3, 3, 26),
+(25, '', 'Patricia', 'Mendoza', 'Femenino', '2001-09-04', 3, 3, 28),
+(26, '', 'Manuel', 'Serrano', 'Masculino', '1999-02-20', 1, 1, 10),
+(27, '', 'Victoria', 'Álvarez', 'Femenino', '2000-04-25', 1, 1, 14),
+(28, '', 'Julio', 'Paredes', 'Masculino', '2001-06-30', 1, 1, 16),
+(29, '', 'Natalia', 'Mora', 'Femenino', '2003-08-15', 1, 1, 17),
+(30, '', 'Diego', 'Velasco', 'Masculino', '2002-10-12', 6, 6, 19);
 
 -- --------------------------------------------------------
 
@@ -190,6 +440,19 @@ CREATE TABLE `horario` (
   `id_asignatura` int(5) NOT NULL,
   `id_hora` int(5) NOT NULL,
   `id_dias` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `planificacion`
+--
+
+CREATE TABLE `planificacion` (
+  `id_planificacion` int(5) NOT NULL,
+  `id_curso` int(5) NOT NULL,
+  `periodo` int(11) NOT NULL,
+  `archivo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -336,16 +599,15 @@ ALTER TABLE `asignatura`
 --
 ALTER TABLE `calificaciones`
   ADD PRIMARY KEY (`id_calificaciones`),
-  ADD KEY `id_asignatura` (`id_asignatura`),
-  ADD KEY `id_estudiante` (`id_estudiante`);
+  ADD KEY `id_estudiante` (`id_estudiante`),
+  ADD KEY `id_asignatura` (`id_asignatura`);
 
 --
 -- Indices de la tabla `calificacion_final`
 --
 ALTER TABLE `calificacion_final`
   ADD PRIMARY KEY (`id_asignatura`),
-  ADD KEY `id_estudiante` (`id_estudiante`),
-  ADD KEY `id_calificaciones` (`id_calificaciones`);
+  ADD KEY `id_estudiante` (`id_estudiante`);
 
 --
 -- Indices de la tabla `cursos`
@@ -387,6 +649,13 @@ ALTER TABLE `horario`
   ADD KEY `id_dias` (`id_dias`);
 
 --
+-- Indices de la tabla `planificacion`
+--
+ALTER TABLE `planificacion`
+  ADD PRIMARY KEY (`id_planificacion`),
+  ADD KEY `id_curso` (`id_curso`);
+
+--
 -- Indices de la tabla `profesores`
 --
 ALTER TABLE `profesores`
@@ -426,7 +695,7 @@ ALTER TABLE `asignatura`
 -- AUTO_INCREMENT de la tabla `calificaciones`
 --
 ALTER TABLE `calificaciones`
-  MODIFY `id_calificaciones` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_calificaciones` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=212;
 
 --
 -- AUTO_INCREMENT de la tabla `calificacion_final`
@@ -465,10 +734,88 @@ ALTER TABLE `horario`
   MODIFY `id_horario` int(5) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de la tabla `planificacion`
+--
+ALTER TABLE `planificacion`
+  MODIFY `id_planificacion` int(5) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `profesores`
 --
 ALTER TABLE `profesores`
   MODIFY `id_profesor` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id_roles` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `tutor`
+--
+ALTER TABLE `tutor`
+  MODIFY `id_tutor` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `asignatura`
+--
+ALTER TABLE `asignatura`
+  ADD CONSTRAINT `asignatura_ibfk_1` FOREIGN KEY (`id_asignatura`) REFERENCES `calificaciones` (`id_calificaciones`);
+
+--
+-- Filtros para la tabla `calificaciones`
+--
+ALTER TABLE `calificaciones`
+  ADD CONSTRAINT `calificaciones_ibfk_1` FOREIGN KEY (`id_estudiante`) REFERENCES `estudiante` (`id_estudiante`);
+
+--
+-- Filtros para la tabla `calificacion_final`
+--
+ALTER TABLE `calificacion_final`
+  ADD CONSTRAINT `calificacion_final_ibfk_1` FOREIGN KEY (`id_asignatura`) REFERENCES `asignatura` (`id_asignatura`),
+  ADD CONSTRAINT `calificacion_final_ibfk_2` FOREIGN KEY (`id_estudiante`) REFERENCES `estudiante` (`id_estudiante`);
+
+--
+-- Filtros para la tabla `estudiante`
+--
+ALTER TABLE `estudiante`
+  ADD CONSTRAINT `estudiante_ibfk_1` FOREIGN KEY (`id_tutor`) REFERENCES `tutor` (`id_tutor`),
+  ADD CONSTRAINT `estudiante_ibfk_3` FOREIGN KEY (`id_profesor`) REFERENCES `profesores` (`id_profesor`),
+  ADD CONSTRAINT `estudiante_ibfk_4` FOREIGN KEY (`id_curso`) REFERENCES `cursos` (`id_curso`),
+  ADD CONSTRAINT `estudiante_ibfk_5` FOREIGN KEY (`id_profesor`) REFERENCES `profesores` (`id_profesor`);
+
+--
+-- Filtros para la tabla `horario`
+--
+ALTER TABLE `horario`
+  ADD CONSTRAINT `horario_ibfk_1` FOREIGN KEY (`id_hora`) REFERENCES `hora` (`id_hora`),
+  ADD CONSTRAINT `horario_ibfk_2` FOREIGN KEY (`id_profesor`) REFERENCES `profesores` (`id_profesor`),
+  ADD CONSTRAINT `horario_ibfk_3` FOREIGN KEY (`id_curso`) REFERENCES `cursos` (`id_curso`),
+  ADD CONSTRAINT `horario_ibfk_4` FOREIGN KEY (`id_asignatura`) REFERENCES `asignatura` (`id_asignatura`),
+  ADD CONSTRAINT `horario_ibfk_5` FOREIGN KEY (`id_dias`) REFERENCES `dias` (`id_dias`);
+
+--
+-- Filtros para la tabla `profesores`
+--
+ALTER TABLE `profesores`
+  ADD CONSTRAINT `profesores_ibfk_1` FOREIGN KEY (`id_asignatura`) REFERENCES `asignatura` (`id_asignatura`);
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_roles`) REFERENCES `roles` (`id_roles`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
